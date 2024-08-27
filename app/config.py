d11a4fa17c50b83a77493ed3a0c42666a92e5aa6
vikram-vivek <49717487+vikram-vivek@users.noreply.github.com>
2024-08-27 15:56:26 +0530

    typo fix HS256

--- app/config.py
+++ app/config.py
@@ -21,7 +21,7 @@ PORT = os.getenv("PORT", "8000")
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
 
 SECRET_KEY = os.getenv("SECRET_KEY", "local")
-ALGORITHM = os.getenv("ALGORITHM", "H256")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 3000))
 
 REDIS_URL = os.getenv("REDIS_URL")
